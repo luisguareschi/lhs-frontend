1448d9b76f0b71d4335529ccb2d7af93751b69cb
luisguareschi <lg@tsl.io>
2024-11-22 18:39:19 +0900

    Added prettier for tailwind

--- components/sections/HeroSection/index.tsx
+++ components/sections/HeroSection/index.tsx
@@ -2,20 +2,20 @@ import { Button } from "@/components/ui/button";
 import Navbar from "@/components/common/navbar/navbar";
 const HeroSection = () => {
   return (
-    <div className="w-full h-screen bg-slate-50 flex flex-col justify-center items-center relative">
-      <div className="w-full absolute top-0">
+    <div className="relative flex h-screen w-full flex-col items-center justify-center bg-slate-50">
+      <div className="absolute top-0 w-full">
         <Navbar />
       </div>
-      <div className="flex justify-center items-center p-20 pt-36 gap-5 md:p-5 max-w-[1500px]">
-        <div className="flex flex-col gap-7 h-full w-full md:items-center">
-          <h1 className="text-slate-800 text-6xl font-bold uppercase pr-5 xl:text-[2.7rem] xl:leading-[2.7rem] md:pr-0 md:text-4xl md:text-center">
+      <div className="flex max-w-[1500px] items-center justify-center gap-5 p-20 pt-36 md:p-5">
+        <div className="flex h-full w-full flex-col gap-7 md:items-center">
+          <h1 className="pr-5 text-6xl font-bold uppercase text-slate-800 xl:text-[2.7rem] xl:leading-[2.7rem] md:pr-0 md:text-center md:text-4xl">
             1 Luxembourg Hispanic American Services
           </h1>
-          <h2 className="text-slate-700 text-lg font-light text-balance leading-normal xl:text-sm pr-10 md:pr-0 md:text-center">
+          <h2 className="text-balance pr-10 text-lg font-light leading-normal text-slate-700 xl:text-sm md:pr-0 md:text-center">
             Soluciones estratégicas y asesoría especializada para empresas
             globales. Contactanos para mas información.
           </h2>
-          <div className="flex justify-between gap-3 mt-auto h-full">
+          <div className="mt-auto flex h-full justify-between gap-3">
             <div className="flex gap-3">
               <Button className="w-fit" size="lg">
                 Contactanos
@@ -24,10 +24,10 @@ const HeroSection = () => {
                 Conoce mas
               </Button>
             </div>
-            <div className="bg-slate-300 rounded-[2rem] aspect-square w-[15vw] mt-auto md:hidden" />
+            <div className="mt-auto aspect-square w-[15vw] rounded-[2rem] bg-slate-300 md:hidden" />
           </div>
         </div>
-        <div className="bg-slate-300 rounded-[2rem] h-full w-[35vw] min-w-[35vw] md:hidden" />
+        <div className="h-full w-[35vw] min-w-[35vw] rounded-[2rem] bg-slate-300 md:hidden" />
       </div>
     </div>
   );
