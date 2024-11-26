e9cbcbeeb0e306f64d32985bf6d442425b40714f
luisguareschi <lg@tsl.io>
2024-11-26 16:18:16 +0900

    Enhance SendEmailProps interface with detailed documentation for email properties

--- queries/email/types.ts
+++ queries/email/types.ts
@@ -1,3 +1,10 @@
+/**
+ * Interface representing the properties required to send an email
+ * @property {string} fromName - The name of the email sender
+ * @property {string} subject - The subject line of the email
+ * @property {string} content - The main body content of the email
+ * @property {string} fromEmail - The email address of the sender
+ */
 export interface SendEmailProps {
   fromName: string;
   subject: string;
